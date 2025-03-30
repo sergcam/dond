2b31f7aab61618afb462af266ef349eb2b8e166e
Sergio Camacho <sergiocam43@gmail.com>
2025-03-30 13:03:33 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ console.log(boxes);
 
 boxes.forEach(function(elem) {
     elem.addEventListener("click", function(){
-        elem.setAttribute("style","background-image: linear-gradient(to right,#39340d,#6b6b6b, #39340d);")
+        if(elem.getAttribute("class")=="undark") {
+            elem.setAttribute("style","background-image: linear-gradient(to right,#39340d,#6b6b6b, #39340d);");
+            elem.setAttribute("class", "dark");
+        }
+        else if (elem.getAttribute("class")=="dark") {
+            elem.setAttribute("style","background-image: linear-gradient(to right,#f2df4f, #ffffff,#f2df4f);");
+            elem.setAttribute("class", "undark");
+        }
+        
     });
 });
